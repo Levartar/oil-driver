820e80e021d3e4d036e794eaf025e5f954ebdc1d
Levartar <jakob.guenster@gmx.net>
2026-04-23 23:46:17 +0300

    show image during dialog

--- game/ui/ingame/fullscreen_image_viewer.gd
+++ game/ui/ingame/fullscreen_image_viewer.gd
@@ -1,7 +1,8 @@
-extends CanvasLayer
+extends Control
 
 @onready var background = %Background
 @onready var image_display = %ImageDisplay
+var closeable = true
 
 
 func _ready() -> void:
@@ -16,15 +17,20 @@ func show_image(texture: Texture2D) -> void:
 func _input(event: InputEvent) -> void:
 	"""Close on any input"""
 	if event is InputEventMouseButton and event.pressed:
-		queue_free()
+		close_viewer()
 		get_tree().root.set_input_as_handled()
 	elif event.is_action_pressed("ui_cancel"):
-		queue_free()
+		close_viewer()
 		get_tree().root.set_input_as_handled()
 
 
 func _on_gui_input(event: InputEvent) -> void:
 	"""Close on GUI input"""
 	if event is InputEventMouseButton and event.pressed:
-		queue_free()
+		close_viewer()
 		get_tree().root.set_input_as_handled()
+
+func close_viewer() -> void:
+	"""Close the fullscreen viewer"""
+	if closeable:
+		queue_free()
